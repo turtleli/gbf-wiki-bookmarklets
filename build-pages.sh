0ddbd6f59dc6ba48071e2dcdf025395b1947ceac
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2020-04-29 18:59:37 +0200

    Add summons to generated webpage

--- build-pages.sh
+++ build-pages.sh
@@ -5,4 +5,5 @@ cp index.html public/index.html
 sed -i "s/#banner/$(cat dist/banner.js)/" public/index.html
 sed -i "s/#character/$(cat dist/character.js)/" public/index.html
 sed -i "s/#weapon/$(cat dist/weapon.js)/" public/index.html
+sed -i "s/#summon/$(cat dist/summon.js)/" public/index.html
 sed -i "s/#date/$(date)/" public/index.html
