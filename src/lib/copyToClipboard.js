49124a7fc6dfc20d02fba8ee88968cf6f35c5117
Jonathan Li <jonathan.li@hotmail.co.uk>
2023-05-29 00:12:26 +0100

    Use clipboard api to copy

--- src/lib/copyToClipboard.js
+++ src/lib/copyToClipboard.js
@@ -1,12 +1,1 @@
-const copyToClipboard = str => {
-    // Source: https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-};
+const copyToClipboard = str => navigator.clipboard.writeText(str);
